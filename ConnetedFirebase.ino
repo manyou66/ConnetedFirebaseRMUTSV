f376edc610f63a742e65c7288945cb6267e55cea
browniApp <wanpracha.n@rmutsv.ac.th>
2018-01-24 14:07:53 +0700

    Connetec WiFi

--- ConnetedFirebase.ino
+++ ConnetedFirebase.ino
@@ -1,14 +1,46 @@
-//Explicit
+//Add Library
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
 
+
+//Explicit
+//การกำหนดค่าคงที่
+//String wifi_ssid = "";
+#define wifi_ssid "MastreEWTC_2.4G"
+#define wifi_pwd "12345abcde"
 
 
 
 void setup() {
-  // put your setup code here, to run once:
+//  การกำหนดช่องทางในการ monitor 
+  Serial.begin(9600);
+
+//  การกำหนดข้อมูลเกี่ยวกับการเชื่อมต่อ wifi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(wifi_ssid,wifi_pwd);
+  Serial.println("Start Find WiFi");
+  
+  while (WiFi.status()!= WL_CONNECTED) {
+//    ถ้า Condition True จะทำงานที่นี้
+        Serial.print("#");
+        delay(500);
+    }
+
+//    ถ้ามาทำงานตรงนี้ คือ Connected WiFi ได้แล้วเท่านั้น
+    Serial.println();
+    Serial.println("Success Conneted WiFi");
+    Serial.println(WiFi.localIP());
 
 }// setup
 
 void loop() {
-  // put your main code here, to run repeatedly:
+//  Serial.println("Hello Browni");
+//  delay(2000);
+
+  
 
 }//loop
+
+
+
+
