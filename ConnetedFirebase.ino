78ca396b452d66125004c4d4c609203e4189a57a
browniApp <wanpracha.n@rmutsv.ac.th>
2018-01-24 14:56:27 +0700

    Update Firebase

--- ConnetedFirebase.ino
+++ ConnetedFirebase.ino
@@ -9,6 +9,10 @@
 #define wifi_ssid "MastreEWTC_2.4G"
 #define wifi_pwd "12345abcde"
 
+#define firebaseHost "rmutsvnodemcu-c2512.firebaseio.com"
+#define firebaseKey "bmFe0QDnjPruYTYaBv9doTcBO9HG4yd1mPuVLlpv"
+
+int intIndex = 0;
 
 
 void setup() {
@@ -17,7 +21,7 @@ void setup() {
 
 //  การกำหนดข้อมูลเกี่ยวกับการเชื่อมต่อ wifi
   WiFi.mode(WIFI_STA);
-  WiFi.begin(wifi_ssid,wifi_pwd);
+  WiFi.begin(wifi_ssid, wifi_pwd);
   Serial.println("Start Find WiFi");
   
   while (WiFi.status()!= WL_CONNECTED) {
@@ -31,13 +35,30 @@ void setup() {
     Serial.println("Success Conneted WiFi");
     Serial.println(WiFi.localIP());
 
+//    ส่วนของการเชื่อมต่อ Firebase
+    Firebase.begin(firebaseHost, firebaseKey);
+    
+
 }// setup
 
 void loop() {
 //  Serial.println("Hello Browni");
 //  delay(2000);
 
-  
+//    Check Connected Firebase
+    Firebase.setInt("myTimes",intIndex);
+    if (Firebase.failed()) {
+//      เมื่อไหร่ก็ตามที่ไม่สามารถเข้าถึง Firebase ได้
+        Serial.print("Can not Connected Firebase : ");
+        Serial.println(Firebase.error());
+        return;
+      }
+        Serial.print("Sucess Connected Firebase Times ==> ");
+        Serial.println(Firebase.getInt("myTimes"));
+
+        intIndex++;
+        delay(500);
+        
 
 }//loop
 
